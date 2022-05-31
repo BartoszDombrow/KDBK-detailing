2d531a34882e3e592efe30658c4f9cbdf39ee0c3
Bartosz Dombrow <bartosz.dombrow1911@gmail.com>
2022-05-31 14:34:49 +0200

    fixx

--- kdbkdetailing/src/App.jsx
+++ kdbkdetailing/src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
 import List from './components/List/List'
 import About from './views/About/About'
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar/>
-      <About/>
-      <List></List>
-    </>
+      <Routes>
+        <Route path='/' element={<About/>}/>
+        <Route path='/cennik' element={<List/>}/>
+      </Routes>
+    </Router>
   )
 }
 
