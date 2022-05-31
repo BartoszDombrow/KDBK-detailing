a4f3842c9c2ac1d24c49d50dd9f45f0b24c21d2c
Bartosz Dombrow <bartosz.dombrow1911@gmail.com>
2022-05-31 12:55:54 +0200

    Add About page

---
+++ kdbkdetailing/src/views/About/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './about.css'
+
+function About() {
+  return (
+    <div className='container-about'>
+      <div className='image-about'/>
+      <div className='content-about'></div>
+    </div>
+  )
+}
+
+export default About
