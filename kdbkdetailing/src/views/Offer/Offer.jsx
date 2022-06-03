22772b12a83623fc510d5225378f21aa841e7bf0
Bartosz Dombrow <bartosz.dombrow1911@gmail.com>
2022-06-03 23:02:01 +0200

    commit

--- kdbkdetailing/src/views/Offer/Offer.jsx
+++ kdbkdetailing/src/views/Offer/Offer.jsx
@@ -8,12 +8,36 @@ function Offer() {
         <h1 className="header">Co oferujemy?</h1>
       </div>
       <div className="offer-content">
-        <div className="offer-content-div"></div>
-        <div className="offer-content-div"></div>
-        <div className="offer-content-div"></div>
-        <div className="offer-content-div"></div>
-        <div className="offer-content-div"></div>
-        <div className="offer-content-div"></div>
+        <div className="offer-content-div">
+          <div className="offer-content-div-overflow">
+            <p className="offer-p">Mycie ręczne</p>
+          </div>
+        </div>
+        <div className="offer-content-div">
+          <div className="offer-content-div-overflow">
+            <p className="offer-p">Korekta lakieru</p>
+          </div>
+        </div>
+        <div className="offer-content-div">
+          <div className="offer-content-div-overflow">
+            <p className="offer-p">Ochrona lakieru, woskowanie</p>
+          </div>
+        </div>
+        <div className="offer-content-div">
+          <div className="offer-content-div-overflow">
+            <p className="offer-p">Pranie tapicerki</p>
+          </div>
+        </div>
+        <div className="offer-content-div">
+          <div className="offer-content-div-overflow">
+            <p className="offer-p">Pielęgnacja skóry</p>
+          </div>
+        </div>
+        <div className="offer-content-div">
+          <div className="offer-content-div-overflow">
+            <p className="offer-p">Regeneracja reflektorów</p>
+          </div>
+        </div>
       </div>
     </div>
   );
